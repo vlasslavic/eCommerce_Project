64b5fdcb4ce7d0a60a7afe0678b82b57c2db5ce0
Veaceslav Vlas <vlasslavic@gmail.com>
2022-10-31 20:07:51 -0400

    Added Profile Table

--- myride.sql
+++ myride.sql
@@ -3,8 +3,8 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 20, 2022 at 06:30 AM
--- Server version: 10.4.25-MariaDB
+-- Generation Time: Nov 01, 2022 at 01:07 AM
+-- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -24,6 +24,24 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `profile`
+--
+
+CREATE TABLE `profile` (
+  `profile_id` int(11) NOT NULL,
+  `seller_id` int(11) NOT NULL,
+  `business_name` varchar(32) NOT NULL,
+  `description` text DEFAULT NULL,
+  `picture` varchar(20) DEFAULT NULL,
+  `phone` int(11) NOT NULL,
+  `email` varchar(72) DEFAULT NULL,
+  `address` text NOT NULL,
+  `isEnabled` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `seller`
 --
 
@@ -38,8 +56,7 @@ CREATE TABLE `seller` (
 --
 
 INSERT INTO `seller` (`seller_id`, `email`, `password_hash`) VALUES
-(1, 'test@mail.com', '$2y$10$Q8nVbS7mgZs7Tyjpw43Ob.jesryJgA6R1yaqe/tOfCfwxXEobDf9C'),
-(2, 'test1@mail.com', '$2y$10$kefATXfQozYofH1deuWGEeWuGyOBVYoHlRAwcstxFeRMnEhGYjI6a');
+(1, 'test@mail.com', '$2y$10$Q8nVbS7mgZs7Tyjpw43Ob.jesryJgA6R1yaqe/tOfCfwxXEobDf9C');
 
 -- --------------------------------------------------------
 
@@ -49,16 +66,31 @@ INSERT INTO `seller` (`seller_id`, `email`, `password_hash`) VALUES
 
 CREATE TABLE `user` (
   `user_id` int(11) NOT NULL,
-  `email` int(11) NOT NULL,
+  `email` varchar(72) NOT NULL,
   `password_hash` varchar(72) NOT NULL,
   `first_name` varchar(16) NOT NULL,
   `last_name` varchar(16) NOT NULL,
-  `address` text DEFAULT NULL
+  `address` text DEFAULT NULL,
+  `picture` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`user_id`, `email`, `password_hash`, `first_name`, `last_name`, `address`, `picture`) VALUES
+(8, 'user@mail.com', '$2y$10$5VW8wKBMPMqHOE51YeDnAObPc8/vD.BpAEOs8iFIUnLDdStIWXvl2', 'Veaceslav', 'Vlas', '1 Rue Vanier', NULL);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `profile`
+--
+ALTER TABLE `profile`
+  ADD PRIMARY KEY (`profile_id`),
+  ADD KEY `FK Profile to Seller` (`seller_id`);
 
 --
 -- Indexes for table `seller`
@@ -79,16 +111,32 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `profile`
+--
+ALTER TABLE `profile`
+  MODIFY `profile_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `seller`
 --
 ALTER TABLE `seller`
-  MODIFY `seller_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `seller_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `profile`
+--
+ALTER TABLE `profile`
+  ADD CONSTRAINT `FK Profile to Seller` FOREIGN KEY (`seller_id`) REFERENCES `seller` (`seller_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
