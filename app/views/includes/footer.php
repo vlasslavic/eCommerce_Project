481772f830ace0ff2a56d9ab93c8c745c4ccb614
Veaceslav Vlas <vlasslavic@gmail.com>
2022-11-22 03:36:53 -0500

    Nav/Footer Language Option

--- app/views/includes/footer.php
+++ app/views/includes/footer.php
@@ -1,6 +1,31 @@
 </main>
 <footer class="" >
     <div class=" my-4 d-flex justify-content-center">
-            <div class=""> © 2022 <span class="fw-bold">myRide</span> team <a target="_blank" href="https://github.com/vlasslavic/eCommerce_Project" class="fw-normal text-black">BootstrapFTW</a></div>
+            <div class="d-flex mt-1">© 2022<span class="fw-bold ms-1 ">myRide</span> <span class='ms-1'> team</span>  <a target="_blank" href="https://github.com/vlasslavic/eCommerce_Project" class="fw-normal ms-1 text-black">BootstrapFTW</a>
+            </div>
+            <?php echo'
+            <ul class="nav nav-pills nav-fill  col-md-auto mb-2 mt-n5 fs-6 align-top mb-md-0 text-decoration-none">
+                <li class="nav-item dropdown  ">
+                <a class=" nav-link dropdown-toggle text-decoration-none text-black fs-6 fw-bold" data-bs-toggle="dropdown"  role="button" aria-expanded="false"><i class="bi bi-globe2"></i><a/>
+                <ul class="dropdown-menu ">
+                    <li><a class="dropdown-item text-decoration-none text-dark" href="'.URLROOT.'Service/addService">
+                    <img class=" ls-is-cached lazyloaded" src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/60/openmoji/338/flag-canada_1f1e8-1f1e6.png" data-src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/60/openmoji/338/flag-canada_1f1e8-1f1e6.png" data-srcset="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/openmoji/338/flag-canada_1f1e8-1f1e6.png 2x" alt="Flag: Canada on OpenMoji " width="20" height="20" srcset="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/openmoji/338/flag-canada_1f1e8-1f1e6.png 2x"
+                    class="my-auto">
+                    CA - English</a></li>
+                    <li><a class="dropdown-item text-decoration-none text-dark" href="'.URLROOT.'Service/list">
+                    <img src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/openmoji/338/flag-for-quebec-caqc_1f3f4-e0063-e0061-e0071-e0063-e007f.png" srcset="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/openmoji/338/flag-for-quebec-caqc_1f3f4-e0063-e0061-e0071-e0063-e007f.png 2x" alt="Flag for Quebec (CA-QC) on OpenMoji 14.0" 
+                        class="my-auto" width="20" height="20">
+                        CA - Francais</a></li>
+                    <li><a class="dropdown-item text-decoration-none text-dark" href="'.URLROOT.'Service/list">
+                    <img class=" ls-is-cached lazyloaded" src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/60/openmoji/338/flag-united-states_1f1fa-1f1f8.png" data-src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/60/openmoji/338/flag-united-states_1f1fa-1f1f8.png" data-srcset="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/openmoji/338/flag-united-states_1f1fa-1f1f8.png 2x" alt="Flag: United States on OpenMoji " width="20" height="20" srcset="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/openmoji/338/flag-united-states_1f1fa-1f1f8.png 2x"
+                    class="my-auto">
+                    US - English</a></li>
+                    
+                    </ul>
+                </li>
+            </ul>
+
+            ';?>
+        
     </div>
 </footer>
--- app/views/includes/navigation.php
+++ app/views/includes/navigation.php
@@ -14,6 +14,25 @@
           <li><a href="#" class="nav-link text-decoration-none p-2 mx-2 '.((str_contains($escaped_url, "/Shops"))?'bg-warning text-black active':'text-white').'">Shops</a></li>
           <li><a href="#" class="nav-link text-decoration-none p-2 mx-2 '.((str_contains($escaped_url, "/FAQs"))?'bg-warning text-black active':'text-white').'">FAQs</a></li>
           <li><a href="'.URLROOT.'About/index'.'" class="nav-link text-decoration-none p-2 mx-2 '.((str_contains($escaped_url, "/Main/about"))?'bg-warning text-black active':'text-white').'">About</a></li>
+          
+          <li class="nav-item dropdown p-1 ">
+              <a class=" nav-link dropdown-toggle text-decoration-none text-light  fs-6 fw-bold" data-bs-toggle="dropdown"  role="button" aria-expanded="false"><i class="bi bi-globe2"></i><a/>
+              <ul class="dropdown-menu ">
+                <li><a class="dropdown-item text-decoration-none text-dark" href="'.URLROOT.'Service/addService">
+                <img class=" ls-is-cached lazyloaded" src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/60/openmoji/338/flag-canada_1f1e8-1f1e6.png" data-src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/60/openmoji/338/flag-canada_1f1e8-1f1e6.png" data-srcset="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/openmoji/338/flag-canada_1f1e8-1f1e6.png 2x" alt="Flag: Canada on OpenMoji " width="20" height="20" srcset="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/openmoji/338/flag-canada_1f1e8-1f1e6.png 2x"
+                class="my-auto">
+                CA - English</a></li>
+                <li><a class="dropdown-item text-decoration-none text-dark" href="'.URLROOT.'Service/list">
+                <img src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/openmoji/338/flag-for-quebec-caqc_1f3f4-e0063-e0061-e0071-e0063-e007f.png" srcset="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/openmoji/338/flag-for-quebec-caqc_1f3f4-e0063-e0061-e0071-e0063-e007f.png 2x" alt="Flag for Quebec (CA-QC) on OpenMoji 14.0" 
+                  class="my-auto" width="20" height="20">
+                  CA - Francais</a></li>
+                <li><a class="dropdown-item text-decoration-none text-dark" href="'.URLROOT.'Service/list">
+                <img class=" ls-is-cached lazyloaded" src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/60/openmoji/338/flag-united-states_1f1fa-1f1f8.png" data-src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/60/openmoji/338/flag-united-states_1f1fa-1f1f8.png" data-srcset="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/openmoji/338/flag-united-states_1f1fa-1f1f8.png 2x" alt="Flag: United States on OpenMoji " width="20" height="20" srcset="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/openmoji/338/flag-united-states_1f1fa-1f1f8.png 2x"
+                class="my-auto">
+                US - English</a></li>
+                
+              </ul>
+            </li>
           '?>
         </ul>
         
