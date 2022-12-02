96d32270317ac1d0c45c6c321a81e7edc5372201
antho-mastro <anthony.mastronardi2@gmail.com>
2022-12-02 13:22:43 -0500

    Update contact.php
    edited contact us

--- app/views/Main/contact.php
+++ app/views/Main/contact.php
@@ -21,24 +21,22 @@
   </div>
   <div class="col-md-4">
     <b>Customer service:</b> <br />
-    Phone: +1 129 209 291<br />
+    Phone: +1 514-574-9744<br />
     E-mail: <a href="mailto:support@mysite.com">support@mysite.com</a><br />
-    <br /><br />
+    <br />
     <b>Headquarter:</b><br />
-    Company Inc, <br />
-    Las vegas street 201<br />
-    55001 Nevada, USA<br />
-    Phone: +1 145 000 101<br />
+    Vanier College, <br />
+    821 Av. Sainte-Croix, Saint-Laurent<br />
+    Phone: +1 514-574-9744<br />
     <a href="mailto:usa@mysite.com">usa@mysite.com</a><br />
 
 
-    <br /><br />
-    <b>Hong kong:</b><br />
-    Company HK Litd, <br />
-    25/F.168 Queen<br />
-    Wan Chai District, Hong Kong<br />
-    Phone: +852 129 209 291<br />
-    <a href="mailto:hk@mysite.com">hk@mysite.com</a><br />
+    <br />
+    <b>Canada:</b><br />
+    Company My Ride, <br />
+    Canada, Quebec <br />
+    Phone: +1 514-574-9744<br />
+    <a href="mailto:hk@mysite.com">Qc@mysite.com</a><br />
 
 
   </div>
