7240ffbf1322d40e7702bdc79afc8486f052817c
Veaceslav Vlas <vlasslavic@gmail.com>
2022-12-05 07:43:32 -0500

    Nav Fix

--- app/views/includes/footer.php
+++ app/views/includes/footer.php
@@ -14,15 +14,15 @@
                 <li class="nav-item dropdown  ">
                 <a class=" nav-link dropdown-toggle text-decoration-none text-black fs-6 fw-bold" data-bs-toggle="dropdown"  role="button" aria-expanded="false"><i class="bi bi-globe2"></i><a/>
                 <ul class="dropdown-menu ">
-                    <li><a class="dropdown-item text-decoration-none text-dark" href="'.URLROOT.'Service/addService">
+                    <li><a class="dropdown-item text-decoration-none text-dark" href="">
                     <img class=" ls-is-cached lazyloaded" src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/60/openmoji/338/flag-canada_1f1e8-1f1e6.png" data-src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/60/openmoji/338/flag-canada_1f1e8-1f1e6.png" data-srcset="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/openmoji/338/flag-canada_1f1e8-1f1e6.png 2x" alt="Flag: Canada on OpenMoji " width="20" height="20" srcset="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/openmoji/338/flag-canada_1f1e8-1f1e6.png 2x"
                     class="my-auto">
                     CA - English</a></li>
-                    <li><a class="dropdown-item text-decoration-none text-dark" href="'.URLROOT.'Service/list">
+                    <li><a class="dropdown-item text-decoration-none text-dark" href="">
                     <img src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/openmoji/338/flag-for-quebec-caqc_1f3f4-e0063-e0061-e0071-e0063-e007f.png" srcset="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/openmoji/338/flag-for-quebec-caqc_1f3f4-e0063-e0061-e0071-e0063-e007f.png 2x" alt="Flag for Quebec (CA-QC) on OpenMoji 14.0" 
                         class="my-auto" width="20" height="20">
                         CA - Francais</a></li>
-                    <li><a class="dropdown-item text-decoration-none text-dark" href="'.URLROOT.'Service/list">
+                    <li><a class="dropdown-item text-decoration-none text-dark" href="">
                     <img class=" ls-is-cached lazyloaded" src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/60/openmoji/338/flag-united-states_1f1fa-1f1f8.png" data-src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/60/openmoji/338/flag-united-states_1f1fa-1f1f8.png" data-srcset="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/openmoji/338/flag-united-states_1f1fa-1f1f8.png 2x" alt="Flag: United States on OpenMoji " width="20" height="20" srcset="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/openmoji/338/flag-united-states_1f1fa-1f1f8.png 2x"
                     class="my-auto">
                     US - English</a></li>
